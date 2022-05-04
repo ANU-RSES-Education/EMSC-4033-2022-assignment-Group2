1104704a28b08ab5b72383077cd3480970db5eea
Dzaky Irfansyah <dzaky.irfansyah@gmail.com>
2022-05-04 00:10:26 +0000

    fix symbolic link

--- User4/tests/test_functions.py
+++ User4/tests/test_functions.py
@@ -1,6 +1,10 @@
 import pytest
 from src.functions import *
 
-def test_foo_function(rtol=1.e-13):
-    result = foo_function(4) - 16
+def test_function_try1(rtol=1.e-7):
+    result = abs(function_try(4) - 24)
     assert result < rtol, " *** error is too big "
+    
+def test_function_try2():
+    function_try(4)
+    assert True, " *** error is too big "
