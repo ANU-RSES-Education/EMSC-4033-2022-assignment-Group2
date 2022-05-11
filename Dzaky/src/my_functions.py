362857cb32aafa1a82b43eaba0b97b183bacd566
Dzaky Irfansyah <dzaky.irfansyah@gmail.com>
2022-05-11 20:53:57 +0000

    Dzaky's function first update

--- Dzaky/src/my_functions.py
+++ Dzaky/src/my_functions.py
@@ -52,113 +52,118 @@ print("salve mundi !")
     return markdown_documentation
 
 
-
 def my_coastlines(resolution):
-    """ returns the relevant coastlines at the requested resolution """
+    """ returns the relevant coastlines following the resolution in input, such as '10m' or '100m' """
 
     import cartopy.feature as cfeature
 
-    return cfeature.NaturalEarthFeature('physcical', 'coastline', res,
-                                        edgecolor=(0.0,0.0,0.0),
-                                        facecolor="none")
+    return cfeature.NaturalEarthFeature('physical', 'coastline', resolution,
+                                        edgecolor=(0.0, 0.0, 0.0),
+                                        facecolor= "none")
 
 
-# def my_water_features(resolution, lakes=True, rivers=True, ocean=False):
-#     """Returns a [list] of cartopy features"""
+def my_water_features(resolution, lakes=True, rivers=True, ocean=False):
+    """Returns a list of cartopy features"""
     
-#     features = []
+    features = [] #create empty list
+
+    river = cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', resolution, facecolor='blue')
+
+    lake = cfeature.NaturalEarthFeature('physical', 'lakes', resolution, facecolor='blue')
+
+    ocean = cfeature.NaturalEarthFeature('physical', 'lakes', resolution, facecolor='blue')
+
+    if rivers == True:
+        features.append(river)
+
+    if lakes == True:
+        features.append(lake)
+
+    if ocean == True:
+        features.append(somethingelse)
+
+    return features
+
+def my_basemaps():
+    """Returns a dictionary of map tile generators that cartopy can use"""
+
+
+    ## The full list of available interfaces is found in the source code for this one:
+    ## https://github.com/SciTools/cartopy/blob/master/lib/cartopy/io/img_tiles.py
+    ## dictionary of possible basemap tile objects
+
+    mapper = {}
+
+    ## Open Street map
+    mapper["open_street_map"] = cimgt.OSM()
+
+    return mapper
+
+
+## specify some point data (e.g. global seismicity in this case)
+
+def download_point_data(area):
+    from obspy.core import event
+    from obspy.clients.fdsn import Client
+    from obspy import UTCDateTime
+
+    client = Client("IRIS")
+
+    # The function input is to define the seismic event observation area
+    starttime = UTCDateTime("2001-01-01")
+    endtime   = UTCDateTime("2006-01-01")
+
+    #Obtain events with specified region
+    cat = client.get_events(starttime=starttime, endtime=endtime, minlatitude=area[1], maxlatitude=area[2], minlongitude=area[3], maxlongitude=area[4], maxdepth=300.00)
+
+    print ("Point data: {} events in catalogue".format(cat.count()))
+
+    ## Unpack the obspy data into a plottable array
+    event_count = cat.count()
+    eq_origin = np.zeros((event_count, 4))
     
-#     if rivers:
-#         features.append(something)
-        
-#     if lakes:
-#         features.append(somethingelse)
+    for i in range(event_count):
+        eq_origin[i][0] = cat[i].origins[0].longitude
+        eq_origin[i][1] = cat[i].origins[0].latitude
+        eq_origin[i][2] = cat[i].origins[0].depth
+        eq_origins[i][3] = cat[i].magnitudes[0].mag
 
-#     if ocean:
-#         features.append(somethingelse)
-    
-#     return features
-
-# def my_basemaps():
-#     """Returns a dictionary of map tile generators that cartopy can use"""
-    
-#     ## The full list of available interfaces is found in the source code for this one:
-#     ## https://github.com/SciTools/cartopy/blob/master/lib/cartopy/io/img_tiles.py
-
-#     # dictionary of possible basemap tile objects
-    
-#     mapper = {}
-    
-#     ## Open Street map
-#     mapper["open_street_map"] = cimgt.OSM()
-
-#     return mapper
+    return eq_origin
 
 
-# ## specify some point data (e.g. global seismicity in this case)
+def my_point_data(area):
+    data = download_point_data(area)
 
-# def download_point_data(region):
-    
-#     from obspy.core import event
-#     from obspy.clients.fdsn import Client
-#     from obspy import UTCDateTime
-
-#     client = Client("IRIS")
-
-#     extent = region
-
-#     starttime = UTCDateTime("1975-01-01")
-#     endtime   = UTCDateTime("2022-01-01")
-    
-#     cat = client.get_events...
-
-#     print ("Point data: {} events in catalogue".format(cat.count()))
-    
-#     # Unpack the obspy data into a plottable array
-
-#     event_count = cat.count()
-
-#     eq_origins = np.zeros((event_count, 4))
-
-#     some_code
-
-#     return eq_origins
+    return data
 
 
-# def my_point_data(region):
-    
-#     data = download_point_data(region)
-    
-#     return data
+## - Some global raster data (lon, lat, data) global plate age, in this example
 
+def download_raster_data():
 
-# ## - Some global raster data (lon, lat, data) global plate age, in this example
-
-# def download_raster_data():
-    
-#     # Seafloor age data and global image - data from Earthbyters
-
-#     # The data come as ascii lon / lat / age tuples with NaN for no data. 
-#     # This can be loaded with ...
+    # Seafloor age data and global image - data from Earthbyters
+    # The data come as ascii lon / lat / age tuples with NaN for no data. 
+    # This can be loaded with ...
 
 #     # age = numpy.loadtxt("Resources/global_age_data.3.6.xyz")
-#     # age_data = age.reshape(1801,3601,3)  # I looked at the data and figured out what numbers to use
+#     # age_data = age.reshape(1801,3601,3)  
+      # I looked at the data and figured out what numbers to use
 #     # age_img  = age_data[:,:,2]
 
 #     # But this is super slow, so I have just stored the Age data on the grid (1801 x 3601) which we can reconstruct easily
 
-#     from cloudstor import cloudstor
-#     teaching_data = cloudstor(url="L93TxcmtLQzcfbk", password='')
-#     teaching_data.download_file_if_distinct("global_age_data.3.6.z.npz", "global_age_data.3.6.z.npz")
+     from cloudstor import cloudstor
+     teaching_data = cloudstor(url="L93TxcmtLQzcfbk", password='')
+     teaching_data.download_file_if_distinct("global_age_data.3.6.z.npz", "global_age_data.3.6.z.npz")
 
-#     datasize = (1801, 3601, 3)
-#     raster_data = np.empty(datasize)
+     datasize = (1801, 3601, 3)
+     raster_data = np.empty(datasize)
 
-#     return raster_data
+     return raster_data
 
 
-# def my_global_raster_data():
+def my_global_raster_data():
 
-#     raster = download_raster_data()
-    
-#     return raster
+     raster = download_raster_data()
+
+     return raster
--- Dzaky/tests/test_functions.py
+++ Dzaky/tests/test_functions.py
@@ -1,10 +1,24 @@
 import pytest
 from src.functions import *
 
-def test_function_try1(rtol=1.e-7):
+def test_basic_function1(rtol=1.e-7):
     result = abs(function_try(4) - 28)
     assert result < rtol, " *** error is too big "
-    
-def test_function_try2():
+
+def test_basic_function2():
     function_try(4)
     assert True, " *** error is too big "
+
+def test_paying_debt():
+    paying_debt(600000)
+    assert True, "*** error is too big"
+
+
+def test_prime_number():
+    count_prime_number(500)
+    assert True, "*** error is too big"
+
+
+def test_city2city():
+    city_to_city()
+    assert True, "*** error is too big"
